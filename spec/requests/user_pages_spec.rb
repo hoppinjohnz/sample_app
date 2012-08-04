c7417cc97cab0a741e4fccdc3eb4b87d401ed9d5
hoppinjohnz <johnzhu00@gmail.com>
2012-08-03 23:23:07 -0400

    all work, onto 10.3.3

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -40,10 +40,10 @@ describe "User pages" do
 
     describe "with valid information" do
       before do
-        fill_in "user_name",         with: "Example User"
-        fill_in "user_email",        with: "user@example.com"
-        fill_in "user_password",     with: "foobar"
-        fill_in "user_password_confirmation", with: "foobar"
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
